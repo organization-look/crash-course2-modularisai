e65e3982147a5061c0ca87379b4980435a270d85
Aldi <aldiponorogo70@gmail.com>
2021-07-02 20:58:53 +0700

    ubah semua

--- user_request.py
+++ user_request.py
@@ -1,2 +1,15 @@
 import requests
-url = requests.get('https://www.detik.com/')
+
+url = 'https://www.detik.com'
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f'sukses{response.status_code}')
+        print(f'Content{response.text}')
+    else:
+        print(f'ada kesalahan{response.status_code}')
+except Exception as e:
+    print(f'error{e}')
+
+print('program end')
